f08b0446f7ad3bd4f688becc94f7fdc3034975b3
Christian Baumann <33223146+chbauman@users.noreply.github.com>
2025-08-03 21:40:38 +0200

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { SectionHeading } from "./common";
 import Cover from "./cover";
 import Footer from "./footer";
@@ -11,12 +12,15 @@ export default function NotFound() {
         <div className="max-w-5xl mx-auto px-4 mt-6">
           <SectionHeading title="404 - Page Not Found" />
           <p className="mb-3 text-gray-700 dark:text-gray-300 text-lg text-center">
-            Oops! The page you're looking for doesn't exist.
+            Oops! The page you&apos;re looking for doesn&apos;t exist.
           </p>
           <p className="mb-3 text-gray-700 dark:text-gray-300 text-lg text-center">
-            <a href="/" className="text-blue-600 underline hover:text-blue-800">
+            <Link
+              href="/"
+              className="text-blue-600 underline hover:text-blue-800"
+            >
               Go back home
-            </a>
+            </Link>
           </p>
         </div>
       </main>
